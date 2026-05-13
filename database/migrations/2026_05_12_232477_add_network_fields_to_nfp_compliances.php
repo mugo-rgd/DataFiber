053e2af4d57cdebbcdaf6187e8f91bb232c22590
mugo-rgd <pmpaulmugo9@gmail.com>
2026-05-13 14:23:24 +0300

    migration5

--- database/migrations/2026_05_12_232477_add_network_fields_to_nfp_compliances.php
+++ database/migrations/2026_05_12_232477_add_network_fields_to_nfp_compliances.php
@@ -10,35 +10,35 @@ return new class extends Migration
     {
         Schema::table('nfp_compliances', function (Blueprint $table) {
             // Add columns without referencing other columns
-            // if (!Schema::hasColumn('nfp_compliances', 'latitude')) {
-            //     $table->decimal('latitude', 10, 7)->nullable();
-            // }
+            if (!Schema::hasColumn('nfp_compliances', 'latitude')) {
+                $table->decimal('latitude', 10, 7)->nullable();
+            }
 
-            // if (!Schema::hasColumn('nfp_compliances', 'longitude')) {
-            //     $table->decimal('longitude', 10, 7)->nullable();
-            // }
+            if (!Schema::hasColumn('nfp_compliances', 'longitude')) {
+                $table->decimal('longitude', 10, 7)->nullable();
+            }
 
-            // if (!Schema::hasColumn('nfp_compliances', 'fibre_km')) {
-            //     $table->decimal('fibre_km', 10, 2)->default(0);
-            // }
+            if (!Schema::hasColumn('nfp_compliances', 'fibre_km')) {
+                $table->decimal('fibre_km', 10, 2)->default(0);
+            }
 
-            // if (!Schema::hasColumn('nfp_compliances', 'tower_count')) {
-            //     $table->integer('tower_count')->default(0);
-            // }
+            if (!Schema::hasColumn('nfp_compliances', 'tower_count')) {
+                $table->integer('tower_count')->default(0);
+            }
              $table->json('infrastructure')->nullable(); // For storing dynamic infrastructure data
     $table->json('primary_numbers')->nullable();
     $table->json('secondary_numbers')->nullable();
     $table->json('bulk_sms')->nullable();
     $table->json('broadband')->nullable();
     $table->json('staff')->nullable();
-   
+
         });
     }
 
     public function down()
     {
         Schema::table('nfp_compliances', function (Blueprint $table) {
-            $columns = ['infrastructure', 'primary_numbers', 'secondary_numbers', 'bulk_sms','broadband','staff'];
+            $columns = ['latitude','longitude','fibre_km','tower_count','infrastructure', 'primary_numbers', 'secondary_numbers', 'bulk_sms','broadband','staff'];
             $table->dropColumn($columns);
         });
     }
